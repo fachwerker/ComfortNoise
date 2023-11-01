d9ee2090a21d3297baaf1d9785938fcee07bb792
fachwerker <63846096+fachwerker@users.noreply.github.com>
2023-11-01 17:10:34 +0100

    moved audio handling stuff to audio service class

---
+++ app/src/main/java/com/example/comfortnoise/AudioService.kt
@@ -0,0 +1,114 @@
+package com.example.comfortnoise
+
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
+import java.util.Random
+
+class AudioService(spectogramView: CanvasSpectogram) {
+
+    private var _spectogramView: CanvasSpectogram = spectogramView
+
+    // synthesize sound
+    lateinit var Track: AudioTrack
+    var isPlaying: Boolean = false
+    val Fs: Int = 44100
+    //val buffLength: Int = AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
+    val noiseLength: Int = Fs*1 // 5s
+    val buffLength: Int = 4096//AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
+
+    // fft
+    val WS = 2048 //  WS = window size
+    lateinit var fftObj: FFT
+    init {
+
+        fftObj = FFT(WS,Fs.toDouble())
+    }
+
+    fun startAudioThread(whiteNoise: Int)
+    {
+
+        Thread {
+            initTrack()
+            startPlaying()
+            playback()
+        }.start()
+    }
+    private fun initTrack() {
+        // Very similar to opening a stream in PyAudio
+        // In Android create a AudioTrack instance and initialize it with different parameters
+
+        // AudioTrack is deprecated for some android versions
+        // Please look up for other alternatives if this does not work
+        Track = AudioTrack(
+            AudioManager.MODE_NORMAL, Fs, AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT, buffLength, AudioTrack.MODE_STREAM
+        )
+    }
+
+    private fun playback( ) {
+        // simple sine wave generator
+        val frame_out: ShortArray = ShortArray(buffLength)
+        val amplitude: Int = 32767
+        val frequency: Int = 3000
+        val twopi: Double = 8.0 * Math.atan(1.0)
+        var phase: Double = 0.0
+
+        val sampleSize = noiseLength // Anzahl der Rauschwerte
+        val minValue = 0 // Minimaler Wert des Rauschens
+        val maxValue = amplitude // Maximaler Wert des Rauschens
+
+        val random = Random() // Initialisiere den Zufallsgenerator mit einer Seed (kann angepasst werden)
+
+        // Generiere das weiße Rauschen
+        val signal = DoubleArray(sampleSize) { random.nextGaussian()*Short.MAX_VALUE*0.1F }//(sampleSize) { random.nextGaussian()*Short.MAX_VALUE*0.1F }.toDoubleArray()
+        //val signal = DoubleArray(sampleSize) //List(sampleSize) { 0.0 }.toMutableList()
+        /*val bandPass = BandPass(100f, 3000f, Fs.toFloat())
+        bandPass.*/
+        for (i in 0 until sampleSize) {
+            signal[i] = (amplitude * Math.sin(phase))
+            phase += twopi * (frequency) / Fs
+            if (phase > twopi) {
+                phase -= twopi
+            }
+        }
+
+
+        val plotData = fftObj.printSpectrogram(signal)
+
+        var idxNoise = 0;
+        var idxPlot = 0;
+        while (isPlaying) {
+            for (i in 0 until buffLength) {
+                /*frame_out[i] = (amplitude * Math.sin(phase)).toInt().toShort()
+                phase += twopi * frequency / Fs
+                if (phase > twopi) {
+                    phase -= twopi
+                }*/
+                frame_out[i] = signal[idxNoise%sampleSize].toInt().toShort()
+                idxNoise++
+                //frame_out[i] = whiteNoise[i]
+            }
+            Track.write(frame_out, 0, buffLength)
+
+            _spectogramView.drawSpectogram(plotData[idxPlot%plotData.size])
+            idxPlot++
+        }
+    }
+
+
+
+    private fun startPlaying() {
+        Track.play()
+        isPlaying = true
+    }
+
+    fun stopPlaying() {
+        if (isPlaying) {
+            isPlaying = false
+            // Stop playing the audio data and release the resources
+            Track.stop()
+            Track.release()
+        }
+    }
+}
--- app/src/main/java/com/example/comfortnoise/MainActivity.kt
+++ app/src/main/java/com/example/comfortnoise/MainActivity.kt
@@ -30,15 +30,15 @@ class MainActivity : AppCompatActivity() {
     val Fs: Int = 44100
     //val buffLength: Int = AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
     val noiseLength: Int = Fs*1 // 5s
-    val buffLength: Int = AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
+    val buffLength: Int = 4096//AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
 
 
-    // fft
-    val WS = 2048 //WS = window size
-    lateinit var fftObj: FFT
 
     // Canvas
     private lateinit var spectogramview: CanvasSpectogram
+
+
+    private lateinit var audioService: AudioService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,13 +47,14 @@ class MainActivity : AppCompatActivity() {
 
         //  init sound pool
         initSoundPool()
-
-
-        streamID = soundPool!!.load(this, R.raw.white_noise, 1)
         binding = ActivityMainBinding.inflate(layoutInflater)
         spectogramview = binding.myCanvas
-        fftObj = FFT(WS,Fs.toDouble())
         setContentView(binding.root)
+
+        audioService = AudioService(spectogramview)
+
+        /*streamID = soundPool!!.load(this, R.raw.white_noise, 1)
+
         binding.WhiteNoise.setOnCheckedChangeListener() { compoundButton: CompoundButton, isChecked: Boolean ->
             if (isChecked) {
                 compoundButton.setBackgroundColor(Color.GREEN)
@@ -64,24 +65,29 @@ class MainActivity : AppCompatActivity() {
                 soundPool?.autoPause()
 //                soundPool?.stop(streamID!!)
             }
-        }
+        }*/
 
+
+        binding.WhiteNoise.setOnCheckedChangeListener() { compoundButton: CompoundButton, isChecked: Boolean ->
+            if (isChecked) {
+                compoundButton.setBackgroundColor(Color.GREEN)
+                audioService.startAudioThread(R.raw.white_noise)
+            } else {
+                compoundButton.setBackgroundColor(Color.RED)
+                audioService.stopPlaying()
+            }
+        }
 
         binding.PinkNoise.setOnCheckedChangeListener() { compoundButton: CompoundButton, isChecked: Boolean ->
             if (isChecked) {
                 compoundButton.setBackgroundColor(Color.GREEN)
-                Thread {
-                    initTrack()
-                    startPlaying()
-                    playback(spectogramview)
-                }.start()
+                audioService.startAudioThread(R.raw.white_noise)
             } else {
                 compoundButton.setBackgroundColor(Color.RED)
-                stopPlaying()
+                audioService.stopPlaying()
             }
         }
     }
-
 
     private fun initSoundPool() {
         if (soundPool == null) {
@@ -108,71 +114,6 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun playback(spectogramview: CanvasSpectogram) {
-        // simple sine wave generator
-        val frame_out: ShortArray = ShortArray(buffLength)
-        val amplitude: Int = 32767
-        val frequency: Int = 3000
-        val twopi: Double = 8.0 * Math.atan(1.0)
-        var phase: Double = 0.0
-
-        val sampleSize = noiseLength // Anzahl der Rauschwerte
-        val minValue = 0 // Minimaler Wert des Rauschens
-        val maxValue = amplitude // Maximaler Wert des Rauschens
-
-        val random = Random() // Initialisiere den Zufallsgenerator mit einer Seed (kann angepasst werden)
-
-        // Generiere das weiße Rauschen
-        val signal = DoubleArray(sampleSize) { random.nextGaussian()*Short.MAX_VALUE*0.1F }//(sampleSize) { random.nextGaussian()*Short.MAX_VALUE*0.1F }.toDoubleArray()
-        //val signal = DoubleArray(sampleSize) //List(sampleSize) { 0.0 }.toMutableList()
-        /*val bandPass = BandPass(100f, 3000f, Fs.toFloat())
-        bandPass.*/
-        for (i in 0 until sampleSize) {
-            signal[i] = (amplitude * Math.sin(phase))
-            phase += twopi * (frequency) / Fs
-            if (phase > twopi) {
-                phase -= twopi
-            }
-        }
-
-
-        val plotData = fftObj.printSpectrogram(signal)
-
-        var idxNoise = 0;
-        var idxPlot = 0;
-        while (isPlaying) {
-            for (i in 0 until buffLength) {
-                /*frame_out[i] = (amplitude * Math.sin(phase)).toInt().toShort()
-                phase += twopi * frequency / Fs
-                if (phase > twopi) {
-                    phase -= twopi
-                }*/
-                frame_out[i] = signal[idxNoise%sampleSize].toInt().toShort()
-                idxNoise++
-                //frame_out[i] = whiteNoise[i]
-            }
-            Track.write(frame_out, 0, buffLength)
-
-            spectogramview.drawSpectogram(plotData[idxPlot%plotData.size])
-            idxPlot++
-        }
-    }
-
-
-
-    private fun startPlaying() {
-        Track.play()
-        isPlaying = true
-    }
-
-    private fun stopPlaying() {
-        if (isPlaying) {
-            isPlaying = false
-            // Stop playing the audio data and release the resources
-            Track.stop()
-            Track.release()
-        }
-    }
 
 
     override fun onDestroy() {
